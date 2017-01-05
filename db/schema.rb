bb97cd65c291ac56eda3ad05823dbe2da2edd26d
Anis ruby <anissoraya@localhost.my>
2017-01-06 02:26:31 +0800

    update local

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170105171557) do
+ActiveRecord::Schema.define(version: 20170105182223) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -55,12 +55,6 @@ ActiveRecord::Schema.define(version: 20170105171557) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-  end
-
-  create_table "day_data", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "items", force: :cascade do |t|

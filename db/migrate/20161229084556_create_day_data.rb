cfc36e3a3f7f04628a40a59b62b65d3908981664
Anis ruby <anissoraya@localhost.my>
2017-01-06 01:05:29 +0800

    update alpha

--- db/migrate/20161229084556_create_day_data.rb
+++ db/migrate/20161229084556_create_day_data.rb
@@ -1,9 +1,12 @@
 class CreateDayData < ActiveRecord::Migration[5.0]
-  def change
+  def self.up
     create_table :day_data do |t|
       t.string :name
 
       t.timestamps
     end
+  end
+  def self.down
+    drop_table :day_data
   end
 end

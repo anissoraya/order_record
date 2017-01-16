0076bebf96c9ac946654e80be921758e43a07733
Anis ruby <anissoraya@localhost.my>
2017-01-17 01:32:17 +0800

    update

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -2,8 +2,11 @@ class PrintController < ApplicationController
   def print_order
     if params[:date] != nil and params[:runner_id] != nil
         @date = Date.parse(params[:date])
-        @orders = Order.where("Date(order_date) = ? AND runner_id = ?", @date, params[:runner_id] )
+        #@orders = Order.where("Date(order_date) = ? AND runner_id = ?", @date, params[:runner_id] )
         @runner = Runner.find(params[:runner_id])
+        @data = Order.where(order_date: @date.midnight..@date.end_of_day, runner_id:params[:runner_id])
+        @sorted = @data.sort_by &:order_date
+
 
     else
       @orders = Order.all

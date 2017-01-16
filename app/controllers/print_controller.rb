0ab5e2516f3bdbbce6d7da5b366c29a3b51aa3d1
Anis ruby <anissoraya@localhost.my>
2017-01-17 02:17:31 +0800

    update

--- app/controllers/print_controller.rb
+++ app/controllers/print_controller.rb
@@ -5,9 +5,11 @@ class PrintController < ApplicationController
         #@orders = Order.where("Date(order_date) = ? AND runner_id = ?", @date, params[:runner_id] )
         @runner = Runner.find(params[:runner_id])
         @data = Order.where(order_date: @date.midnight..@date.end_of_day, runner_id:params[:runner_id])
+        @count = Order.where(order_date: @date.midnight..@date.end_of_day, runner_id:params[:runner_id]).count
+
         @sorted = @data.sort_by &:order_date
 
-
+        @count = @data.uniq.pluck(:customer_id).count
     else
       @orders = Order.all
     end
